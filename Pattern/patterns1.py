767fb0beca1cbcf2af0c54201cb9426644112e19
murlishankar458 <murlishankar458@gmail.com>
2026-09-21 15:31:21 +0530

    Create patterns1.py

---
+++ Pattern/patterns1.py
@@ -0,0 +1,9 @@
+# PATTERN 1 
+
+# n = int(input())
+
+# for i in range(n):
+#     for j in range(n):
+#         print("*", end="") # end * ko rows me lata hai 
+#     print() # print() next line pe jane ke liye hota hai
+
